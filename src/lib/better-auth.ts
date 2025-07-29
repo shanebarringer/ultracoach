2e0fe38654a2623d86e0e1e5183e8b4a50166f1b
Shane Barringer <shanebarringer@gmail.com>
2025-07-29 13:20:54 -0600

    temporarily disables import of nextCookies

--- src/lib/better-auth.ts
+++ src/lib/better-auth.ts
@@ -1,6 +1,6 @@
 import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
-import { nextCookies } from 'better-auth/next-js'
+// import { nextCookies } from 'better-auth/next-js'
 import { drizzle } from 'drizzle-orm/node-postgres'
 import { Pool } from 'pg'
 
